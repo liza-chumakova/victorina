4e9a05d83d99a5ca51d3d3f46a7384e865aee23b
Liza Chumakova <lisa.chumakova@gmail.com>
2021-02-07 17:53:56 +0300

    Implement Idle, Garland and test states

--- src/victorina/victorina.ino
+++ src/victorina/victorina.ino
@@ -22,117 +22,178 @@
  * SOFTWARE.
  */
 
+#include <ezButton.h>
+
 enum VictorinaState {
-  VictorinaStateNull = 0,
-  VictorinaStateIdle,
-  VictorinaStateRun1,
-  VictorinaStateRun2
+  VictorinaStateIdle = 0,
+  VictorinaStateGarland,
+  VictorinaStateTest,
+  VictorinaStateWaitingForWinner,
+  VictorinaStateDisplayWinner
 };
 
-const int LED1 = 13;
-const int LED2 = 12;
-const int LED3 = 11;
-const int LED4 = 10;
+const int PARTICIPANT_1_LED = 13;
+const int PARTICIPANT_2_LED = 12;
+const int PARTICIPANT_3_LED = 11;
+const int PRESENTER_EXTRA_LED = 10;
+const int PRESENTER_KEY_1 = A4;
+const int PRESENTER_KEY_2 = A5;
+const int PARTICIPANT_1_KEY = A1;
+const int PARTICIPANT_2_KEY = A2;
+const int PARTICIPANT_3_KEY = A3;
 
-static unsigned long TheVar = 0;
-static VictorinaState TheVictorinaState = VictorinaStateNull;
-static int TheState = 0;
+
+
+static unsigned long TheGarlandNextSwitchTimeMs = 0;
+static VictorinaState TheVictorinaState = VictorinaStateIdle;
+static int TheGarlandActiveLED = 0;
 static unsigned long TheOldButtonState = 1;
+static ezButton ThePresenterButton1(PRESENTER_KEY_1);
+static ezButton ThePresenterButton2(PRESENTER_KEY_2);
 
 static void update_LEDs();
 static void loop_500ms();
 static void handle_button_press();
+static void loop_idle();
+static void loop_garland();
+static void loop_test();
+static void loop_waiting_for_winner();
+static void loop_display_winner();
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(A5, INPUT);
+  pinMode(PARTICIPANT_1_LED, OUTPUT);
+  pinMode(PARTICIPANT_2_LED, OUTPUT);
+  pinMode(PARTICIPANT_3_LED, OUTPUT);
+  pinMode(PRESENTER_EXTRA_LED, OUTPUT);
+  pinMode(PRESENTER_KEY_2, INPUT);
+  pinMode(PRESENTER_KEY_1, INPUT);
+  pinMode(PARTICIPANT_1_KEY, INPUT);
+  pinMode(PARTICIPANT_2_KEY, INPUT);
+  pinMode(PARTICIPANT_3_KEY, INPUT);
+  ThePresenterButton1.setDebounceTime(50);
+  ThePresenterButton2.setDebounceTime(50);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  unsigned long time_now = millis();
-  if (TheVar == 0)
+void loop()
+{
+  if (VictorinaStateIdle == TheVictorinaState)
   {
-    loop_500ms();
-    TheVar = time_now + 500;
+    loop_idle();
   }
-  else if (time_now >= TheVar)
+  else if (VictorinaStateGarland == TheVictorinaState)
   {
-    loop_500ms();
-    TheVar = time_now + 500;
+    loop_garland();
   }
-  update_LEDs();
-  int but = digitalRead(A5);
-  if (but != TheOldButtonState)
+  else if (VictorinaStateTest == TheVictorinaState)
   {
-    TheOldButtonState = but;
-    if (but == 0)
-    {
-      handle_button_press();
-    }
+    loop_test();
+  }
+  else if (VictorinaStateWaitingForWinner == TheVictorinaState)
+  {
+    loop_waiting_for_winner();
+  }
+  else if (VictorinaStateDisplayWinner == TheVictorinaState)
+  {
+    loop_display_winner();
   }
 }
 
 void loop_500ms()
 {
-#if 0
-  if (TheState == 0)
+  if (0 == TheGarlandActiveLED)
   {
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
-    digitalWrite(LED4, HIGH);
-    TheState = TheState+1;
+    digitalWrite(PARTICIPANT_1_LED, LOW);
+    digitalWrite(PARTICIPANT_2_LED, HIGH);
+    digitalWrite(PARTICIPANT_3_LED, HIGH);
+    digitalWrite(PRESENTER_EXTRA_LED, HIGH);
+    TheGarlandActiveLED = 1;
   }
-  else
+  else if (1 == TheGarlandActiveLED)
   {
-     digitalWrite(LED1, LOW);
-     digitalWrite(LED2, LOW);
-     digitalWrite(LED3, LOW);
-     digitalWrite(LED4, LOW);
-     TheState = 0;
+    digitalWrite(PARTICIPANT_1_LED, HIGH);
+    digitalWrite(PARTICIPANT_2_LED, LOW);
+    digitalWrite(PARTICIPANT_3_LED, HIGH);
+    digitalWrite(PRESENTER_EXTRA_LED, HIGH);
+    TheGarlandActiveLED = 2;
   }
-#endif
-}
-
-void update_LEDs()
-{
-  int rem = TheState%4;
-  if (rem == 0)
+  else if (2 == TheGarlandActiveLED)
   {
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
-    digitalWrite(LED4, HIGH);
-  }
-  else if (rem == 1)
-  {
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, HIGH);
-    digitalWrite(LED4, HIGH);
-  }
-  else if (rem == 2)
-  {
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, LOW);
-    digitalWrite(LED4, HIGH);
-  }
-  else if (rem == 3)
-  {
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
-    digitalWrite(LED4, LOW);
+    digitalWrite(PARTICIPANT_1_LED, HIGH);
+    digitalWrite(PARTICIPANT_2_LED, HIGH);
+    digitalWrite(PARTICIPANT_3_LED, LOW);
+    digitalWrite(PRESENTER_EXTRA_LED, HIGH);
+    TheGarlandActiveLED = 0;
   }
 }
 
 void handle_button_press()
 {
-  TheState = TheState + 1;
+}
+
+void loop_idle()
+{
+  // Даём возможность первой кнопке обработать то, что ей надо
+  ThePresenterButton1.loop();
+
+  // Проверим, надо ли переходить в другие состояния
+  if (ThePresenterButton1.isPressed())
+  {
+    TheVictorinaState = VictorinaStateGarland;
+  }
+
+  digitalWrite(PARTICIPANT_1_LED, HIGH);
+  digitalWrite(PARTICIPANT_2_LED, HIGH);
+  digitalWrite(PARTICIPANT_3_LED, HIGH);
+  digitalWrite(PRESENTER_EXTRA_LED, LOW);
+}
+
+void loop_garland()
+{
+  // Даём возможность первой кнопке обработать то, что ей надо
+  ThePresenterButton1.loop();
+
+  // Проверим, надо ли переходить в другие состояния
+  if (ThePresenterButton1.isPressed())
+  {
+    TheVictorinaState = VictorinaStateTest;
+  }
+
+  const int timeNow = millis();
+  if(0 == TheGarlandNextSwitchTimeMs || TheGarlandNextSwitchTimeMs <= timeNow)
+  {
+    loop_500ms();
+    TheGarlandNextSwitchTimeMs = timeNow + 250;
+  }
+}
+
+void loop_test()
+{
+  // Даём возможность первой кнопке обработать то, что ей надо
+  ThePresenterButton1.loop();
+
+  // Проверим, надо ли переходить в другие состояния
+  if (ThePresenterButton1.isPressed())
+  {
+    TheVictorinaState = VictorinaStateWaitingForWinner;
+  }
+
+  digitalWrite(PARTICIPANT_1_LED, digitalRead(PARTICIPANT_1_KEY));
+  digitalWrite(PARTICIPANT_2_LED, digitalRead(PARTICIPANT_2_KEY));
+  digitalWrite(PARTICIPANT_3_LED, digitalRead(PARTICIPANT_3_KEY));
+}
+
+void loop_waiting_for_winner()
+{
+  digitalWrite(PARTICIPANT_1_LED, HIGH);
+  digitalWrite(PARTICIPANT_2_LED, HIGH);
+  digitalWrite(PARTICIPANT_3_LED, HIGH);
+  digitalWrite(PRESENTER_EXTRA_LED, HIGH);
+}
+
+void loop_display_winner()
+{
+  // Даём возможность первой и второй кнопкам обработать то, что им надо
+  ThePresenterButton1.loop();
+  ThePresenterButton2.loop();
 }
